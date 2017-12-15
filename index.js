252742b15747270b068e85bb3d10e1d7da29c9c2
Cara Kane <cara.kane@gmail.com>
2017-12-15 17:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function createDriver(){
   let driverId = 0;
   return function(){
     class Driver{
-      constructor(){
+      constructor(name){
+        this.name = name
         this.id = ++driverId
       }
     }
